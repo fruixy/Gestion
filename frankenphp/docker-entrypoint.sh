67ee5f0e289c23f0ff7dacc41b68b0f622b6fe42
fruixy <lemonnieralexis19@gmail.com>
2025-04-22 18:48:02 +0200

    add templates

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -7,8 +7,8 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	fi
 
 	php bin/console importmap:install
-	php bin/console assets:install public
 	php bin/console asset-map:compile
+	php bin/console assets:install public
 
 	if grep -q ^DATABASE_URL= .env; then
 		echo "Waiting for database to be ready..."
