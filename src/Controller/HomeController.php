768b9e8ccb995d27c5027e789f3833f87697e5c2
fruixy <lemonnieralexis19@gmail.com>
2025-04-22 20:51:53 +0200

    add controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home_index')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findByLatest();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'articles' => $articles
         ]);
     }
 }
+
