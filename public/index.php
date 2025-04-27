e46d10c2bde1a8514615522d81a7197f2557f89d
fruixy <lemonnieralexis19@gmail.com>
2025-04-27 15:47:20 +0200

    re order

--- public/index.php
+++ public/index.php
@@ -1,20 +1,16 @@
 <?php
-
-use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
-// Correction Railway pour HTTPS
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '' === 'https') {
+    Request::setTrustedProxies(
+        [$_SERVER['REMOTE_ADDR']],
+        Request::HEADER_X_FORWARDED_PROTO
+    );
 }
 
-// Correction complète pour Railway proxies
-Request::setTrustedProxies(
-    [$_SERVER['REMOTE_ADDR']],
-    Request::HEADER_X_FORWARDED_PROTO
-);
+use App\Kernel;
 
-require_once dirname(DIR) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
