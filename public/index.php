dcf5695774c7d6d3b991ad74c0f804f1ad3c0961
fruixy <lemonnieralexis19@gmail.com>
2025-04-27 14:54:20 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '' === 'https') {
+    Request::setTrustedProxies(
+        [$_SERVER['REMOTE_ADDR']],
+        Request::HEADER_X_FORWARDED_PROTO
+    );
+}
 
 use App\Kernel;
 
